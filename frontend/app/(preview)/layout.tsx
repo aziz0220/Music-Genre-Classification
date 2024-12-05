2867de1c1ed610466c9788ff2491a848cf938ebe
aziz0220 <benamoraziz0220@gmail.com>
2024-12-05 11:14:04 +0100

    reduced Jenkins file

--- frontend/app/(preview)/layout.tsx
+++ frontend/app/(preview)/layout.tsx
@@ -7,9 +7,9 @@ import { Geist } from "next/font/google";
 const geist = Geist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://ai-sdk-preview-pdf-support.vercel.app"),
-  title: "PDF Support Preview",
-  description: "Experimental preview of PDF support with the AI SDK",
+  metadataBase: new URL("https://music-classifier.vercel.app"),
+  title: "Music Genre Classifier",
+  description: "classify WAV music files to genres using SVM and VGG",
 };
 
 export default function RootLayout({
